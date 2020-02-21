3d37fb209af6ae5d3b6f3c508c286ff9bc8edf44
Cristian Nieto <cristianandresnieto@gmail.com>
2020-02-21 18:00:07 -0500

    Clean code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 
-/*  if you want use single color theme
-
-- ColorSwitcher component remove in 'src/App.vue' file.
-- Remove multicolor section in current file
-- Uncomment the two lines below
-
-import "./assets/css/sass/themes/piaf.light.orange.scss";
-import "./main";
-*/
-
-/* if you want single color remove this section:multicolor */
-
-
 let render = () => {
   import("./assets/css/sass/themes/piaf.light.blue.scss").then(() =>
     require("./main")
